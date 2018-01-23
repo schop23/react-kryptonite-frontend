9e9260dcfa2645b1432d31f7001def24157493dd
Shelley Schoppert <schop23@gmail.com>
2018-01-23 12:14:33 -0500

    adding form info in Post.js

--- src/Post.js
+++ src/Post.js
@@ -5,7 +5,19 @@ class Post extends Component {
     return(
       <div>
         <form onSubmit={this.props.handlePostSubmit}>
+        
           <input type= 'text' placeholder='Comic Book Title' onChange={this.props.handlePostInput} />
+
+          <input type= 'text' placeholder='Publisher' onChange={this.props.handlePostInput} />
+
+          <input type= 'text' placeholder='Issue Number' onChange={this.props.handlePostInput} />
+
+          <input type= 'text' placeholder='Buy Digital Comic Book' onChange={this.props.handlePostInput} />
+
+          <textarea type='text' placeholder='Write Review Here' onChange={this.props.handlePostInput} />
+
+          <input type='submit' value='Post'/>
+
         </form>
       </div>
     )
